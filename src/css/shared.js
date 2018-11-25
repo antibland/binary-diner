ccf74d50206aad462848f5907fd4346c6834ccd7
Andy Hoffman <antibland@gmail.com>
2018-11-25 02:08:06 -0800

    Mobile styles and styled component sharing

--- src/css/shared.js
+++ src/css/shared.js
@@ -1,10 +1,21 @@
 import styled from 'styled-components';
+import utilities from '../utilities';
 
 const shared = {
   reasonList: () => styled.ol`
     list-style-type: none;
-    margin-left: 1em;
     counter-reset: li-counter;
+    padding: 0;
+    margin: 0 auto;
+    width: calc(100% - 1em);
+
+    ${utilities.media.small`
+      width: calc(100% - 3em);
+    `}
+
+    ${utilities.media.medium`
+      max-width: 60vw;
+    `}
   `,
 
   reasonListItem: () => styled.li`
@@ -50,6 +61,50 @@ const shared = {
       box-shadow: 0 16px 30px -5px rgba(0, 0, 0, 0.4);
       transform: perspective(800px) rotateY(0deg) rotateX(45deg);
     }
+  `,
+
+  stepButton: () => styled.button`
+    overflow: hidden;
+    padding: 12px;
+    cursor: pointer;
+    user-select: none;
+    transition: all 150ms linear;
+    text-align: center;
+    white-space: nowrap;
+    text-transform: capitalize;
+    border: 0 none;
+    border-radius: 4px;
+    font-size: 1rem;
+    font-weight: 500;
+    line-height: 1.3;
+    appearance: none;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 2px 5px 10px var(--smoke);
+    margin: 10px auto;
+    color: #fff;
+    background-color: var(--red);
+
+    &:hover {
+      transition: all 150ms linear;
+      opacity: 0.85;
+    }
+
+    &:active {
+      transition: all 150ms linear;
+      opacity: 0.75;
+    }
+
+    &:focus {
+      outline: 1px dotted #959595;
+      outline-offset: -4px;
+    }
+  `,
+
+  section: () => styled.section`
+    ${utilities.media.medium`
+      padding: 1em;
+    `}
   `,
 };
 
--- src/views/ButtonView.js
+++ src/views/ButtonView.js
@@ -4,39 +4,94 @@ import shared from '../css/shared';
 
 const ReasonList = shared.reasonList();
 const ReasonListItem = shared.reasonListItem();
+const Section = shared.section();
 
 const ButtonView = () => (
   <>
-    <h2>Attempt #1</h2>
-    <Highlight className="html">
-      {`<a href="#" class="button">Click here</a>`}
-    </Highlight>
+    <Section>
+      <h2>Attempt #1</h2>
+      <Highlight className="html">{`<a href="#">Click here</a>`}</Highlight>
 
-    <ul className="log">
-      <li className="entry theirs">
-        <img src="/snob.jpg" alt="" className="avatar" />
-        <p className="message">
-          Enjoy your button, sir. What's that? You don't like it?
-        </p>
-      </li>
-      <li className="entry mine">
-        <img src="/me.jpg" alt="" className="avatar" />
-        <p className="message">I can't eat this! Tell the chef:</p>
-      </li>
-    </ul>
+      <ul className="log">
+        <li className="entry theirs">
+          <img src="/snob.jpg" alt="" className="avatar" />
+          <p className="message">
+            Enjoy your button, sir. What's that? You don't like it?
+          </p>
+        </li>
+        <li className="entry mine">
+          <img src="/me.jpg" alt="" className="avatar" />
+          <p className="message">I can't eat this! Tell the chef:</p>
+        </li>
+      </ul>
 
-    <ReasonList>
-      <ReasonListItem>
-        Accessibility suffers when anchors are used in place of native buttons.
-      </ReasonListItem>
-      <ReasonListItem>
-        Some browsers don't allow users to tab to the anchor.
-      </ReasonListItem>
-      <ReasonListItem>
-        Screen readers don't provide helpful announcements for links meant to be
-        buttons.
-      </ReasonListItem>
-    </ReasonList>
+      <ReasonList>
+        <ReasonListItem>
+          Accessibility suffers when anchors are used in place of native
+          buttons.
+        </ReasonListItem>
+        <ReasonListItem>
+          Some browsers don't allow users to tab to the anchor.
+        </ReasonListItem>
+        <ReasonListItem>
+          Screen readers don't provide helpful announcements for links meant to
+          be buttons.
+        </ReasonListItem>
+      </ReasonList>
+    </Section>
+
+    <Section>
+      <h2>Attempt #2</h2>
+      <Highlight className="html">
+        {`<a tabindex="0" role="button" href="#">Click here</a>`}
+      </Highlight>
+
+      <ul className="log">
+        <li className="entry theirs">
+          <img src="/snob.jpg" alt="" className="avatar" />
+          <p className="message">
+            I've spoken to the chef, and he has made a fresh button for you.
+            Please like us on Yelp.
+          </p>
+        </li>
+        <li className="entry mine">
+          <img src="/me.jpg" alt="" className="avatar" />
+          <p className="message">
+            First, I hate Yelp. More importantly, I still can't eat this code.
+            This is not even a real button. Take these comments back to the
+            chef. Last try before I leave.
+          </p>
+        </li>
+      </ul>
+
+      <ReasonList>
+        <ReasonListItem>
+          Adding a <code>tabindex</code> attribute “fixes” the focus issue, but
+          the <code>&lt;button&gt;</code> receives focus for free.
+        </ReasonListItem>
+        <ReasonListItem>
+          Why play a <code>role</code> when you can be the real thing?
+        </ReasonListItem>
+        <ReasonListItem>
+          Links <em>go</em> places; buttons <em>do</em> things. Simple rule.
+        </ReasonListItem>
+      </ReasonList>
+    </Section>
+
+    <Section>
+      <h2>Attempt #3</h2>
+      <Highlight className="html">{`<button>Click here</button>`}</Highlight>
+
+      <ul className="log">
+        <li className="entry theirs">
+          <img src="/snob.jpg" alt="" className="avatar" />
+          <p className="message">
+            The chef realizes he was doing a lot of needless work. This one is
+            on the house, as they say.
+          </p>
+        </li>
+      </ul>
+    </Section>
   </>
 );
 
